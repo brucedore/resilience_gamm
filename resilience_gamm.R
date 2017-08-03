6f0688f9e11910e6c7414b972296e27e43cd87a0
brucedore <bruce@ASCs-MacBook-Pro-4.local>
2017-08-03 07:29:34 -0400

    ...

--- resilience_gamm.R
+++ resilience_gamm.R
@@ -25,9 +25,9 @@ wid_vivs_bam <- bam(wbeing_wid ~ s(year_wid, k=48) + s(subj_wid, bs="re") + s(su
 emp_vivs_bam <- bam(wbeing_emp ~ s(year_emp, k=48) + s(subj_emp, bs="re") + s(subj_emp, year_emp, bs="re"), data=empred)
 
 #varying curve 
-div_vsm_bam<-bam(wbeing_div ~ s(year_div, k = 48) + s(year_div, subj_wid, bs = "fs"), data=div)
+div_vsm_bam<-bam(wbeing_div ~ s(year_div, k = 48) + s(year_div, subj_div, bs = "fs"), data=div)
 wid_vsm_bam<-bam(wbeing_wid ~ s(year_wid, k = 48) + s(year_wid, subj_wid, bs = "fs"), data=wid)
-emp_vsm_bam<-bam(wbeing_emp ~ s(year_emp, k = 48) + s(year_emp, subj_wid, bs = "fs"), data=empred)
+emp_vsm_bam<-bam(wbeing_emp ~ s(year_emp, k = 48) + s(year_emp, subj_emp, bs = "fs"), data=empred)
 
 
 
