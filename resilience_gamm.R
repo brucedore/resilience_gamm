fd1dc93740c4b9d73c43440b254443d1f4ef2840
brucedore <bruce@ASCs-MacBook-Pro-4.local>
2017-08-03 00:43:21 -0400

    ..

--- resilience_gamm.R
+++ resilience_gamm.R
@@ -10,9 +10,9 @@ wid<-read_csv("~/wid.csv")
 empred<-read_csv("~/empred.csv")
 
 #fixed effect of time, no varying parameters
-div_fe_bam <- wbeing_div ~ s(year_div, k = 48), data=div);
-wid_fe_bam <- wbeing_wid ~ s(year_wid, k = 48), data=wid);
-emp_fe_bam <- wbeing_emp ~ s(year_emp, k = 48), data=empred);
+div_fe_bam <- bam(wbeing_div ~ s(year_div, k = 48), data=div)
+wid_fe_bam <- bam(wbeing_wid ~ s(year_wid, k = 48), data=wid)
+emp_fe_bam <- bam(wbeing_emp ~ s(year_emp, k = 48), data=empred)
 #varying intercept
 div_vi_bam <- bam(wbeing_div~s(year_div, k=48) + s(subj_div, bs="re"), data=div)
 wid_vi_bam <- bam(wbeing_wid~s(year_wid, k=48) + s(subj_wid, bs="re"), data=wid)
@@ -22,9 +22,9 @@ div_vivs_bam <- bam(wbeing_div~s(year_div, k=48) + s(subj_div, bs="re") + s(subj
 wid_vivs_bam <- bam(wbeing_wid~s(year_wid, k=48) + s(subj_wid, bs="re") + s(subj_wid, year_wid, bs="re"), data=wid)
 emp_vivs_bam <- bam(wbeing_emp~s(year_emp, k=48) + s(subj_emp, bs="re") + s(subj_emp, year_emp, bs="re"), data=empred)
 #varying curve 
-div_vsm_bam<-wbeing_div ~ s(year_div, k = 48) + s(year_div, subj_wid, bs = "fs", data=div)
-wid_vsm_bam<-wbeing_wid ~ s(year_wid, k = 48) + s(year_wid, subj_wid, bs = "fs", data=wid)
-emp_vsm_bam<-wbeing_emp~ s(year_emp, k = 48) + s(year_emp, subj_wid, bs = "fs", data=empred)
+div_vsm_bam<-bam(wbeing_div ~ s(year_div, k = 48) + s(year_div, subj_wid, bs = "fs", data=div)
+wid_vsm_bam<-bam(wbeing_wid ~ s(year_wid, k = 48) + s(year_wid, subj_wid, bs = "fs", data=wid)
+emp_vsm_bam<-bam(wbeing_emp~ s(year_emp, k = 48) + s(year_emp, subj_wid, bs = "fs", data=empred)
 
 
 
